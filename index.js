c8a200fc31c31f67bd9ca03aaf8829cdf9cfef66
kaz <kzzz123@protonmail.com>
2023-02-18 22:46:52 +0400

    prettier

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const arweave_wallet = require("./YSReuO6vzkBWd4Tdfe8HE0YLXj4tJokfatsORv7iZ94.json");
-const { rpc, contractTxId } = require("./config");
-const { isNil } = require("ramda");
+const arweave_wallet = require("./YSReuO6vzkBWd4Tdfe8HE0YLXj4tJokfatsORv7iZ94.json")
+const { rpc, contractTxId } = require("./config")
+const { isNil } = require("ramda")
 // const WeaveDB = require ('weavedb-node-client')
 // const sdk = new WeaveDB({
 //   contractTxId: contractTxId,
@@ -11,32 +11,30 @@ const { isNil } = require("ramda");
 //   // rpc: '34.245.150.161:9090',
 // });
 
-const WeaveDB = require("weavedb-sdk-node");
+const WeaveDB = require("weavedb-sdk-node")
 const sdk = new WeaveDB({
   // wallet: arweave_wallet,
   arweave_wallet: arweave_wallet,
   contractTxId: contractTxId,
-});
+})
 // In case the wallet is not set, you can run initializeWithoutWallet() after the instantiation.
 // await sdk.initializeWithoutWallet()
 // // Or you can assign the wallet later. Note initialize() is not an async-function.
 sdk.initialize({
   // wallet: ADMIN_ARWEAVE_WALLET_JSON
   wallet: arweave_wallet,
-});
-(async () => {
-  console.log(`INFO: start.. ${contractTxId}`);
-  console.log(
-    `INFO: url: https://sonar.warp.cc/#/app/contract/${contractTxId}`
-  );
-  const collectionName = "test";
+})
+;(async () => {
+  console.log(`INFO: start.. ${contractTxId}`)
+  console.log(`INFO: url: https://sonar.warp.cc/#/app/contract/${contractTxId}`)
+  const collectionName = "test"
 
   // setRules
   try {
-    const ret1 = await sdk.setRules({ "allow write": true }, collectionName);
-    console.log("ret1.success: ", ret1.success);
+    const ret1 = await sdk.setRules({ "allow write": true }, collectionName)
+    console.log("ret1.success: ", ret1.success)
   } catch (e) {
-    console.log(e.message);
+    console.log(e.message)
   }
 
   // add
@@ -44,27 +42,27 @@ sdk.initialize({
     const ret2 = await sdk.add(
       { text: `hello world ${Date.now()}` },
       collectionName
-    );
-    console.log("ret2['docId']: ", ret2["docID"]);
+    )
+    console.log("ret2['docId']: ", ret2["docID"])
   } catch (e) {
-    console.log(e.message);
+    console.log(e.message)
   }
 
   // get
   try {
-    const list = await sdk.cget(collectionName, 100, true);
-    list.forEach((item) => {
+    const list = await sdk.cget(collectionName, 100, true)
+    list.forEach(item => {
       if (
         isNil(item) ||
         isNil(item.id) ||
         isNil(item.data) ||
         isNil(item.data.text)
       )
-        return;
-      console.log(`${item.id} => ${item.data.text}`);
-    });
+        return
+      console.log(`${item.id} => ${item.data.text}`)
+    })
   } catch (e) {
-    console.log(e.message);
+    console.log(e.message)
   }
-  process.exit();
-})();
+  process.exit()
+})()
